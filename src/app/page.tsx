43a019b008316a18f8ed8b078c2cef6396d50462
Mohamed Hussein <e2001945@edu.vamk.fi>
2023-10-23 23:47:06 +0300

    started on the landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,40 @@
-import MaxWidthWrapper from '@/components/MaxWidthWrapper'
-
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import Link from "next/link";
+import { ArrowRight } from "lucide-react";
+import { buttonVariants } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <MaxWidthWrapper className='bg-red-500'><div>hello world</div></MaxWidthWrapper>
-  )
+    <>
+      <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
+        <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
+          <p className="text-sm font-semibold text-gray-700">Welcome</p>
+        </div>
+        <h1 className="max-w-4xl text-5xl font-bold md:text-6l lg:text-7xl">
+          Chat with your <span className="text-fuchsia-600">documents</span> in
+          seconds
+        </h1>
+        <p className="mt-5 max-w-prose text-slate-100 sm:text-lg">
+          Upload your files and seek information about them.
+        </p>
+
+        <Link
+          className={buttonVariants({
+            size: "lg",
+            className: "mt-5",
+          })}
+          href="/dashboard"
+          target="_blank"
+        >
+          Get started <ArrowRight className="ml-2 h-5 w-5" />
+        </Link>
+      </MaxWidthWrapper>
+
+      <div>
+        <div className="border relative isolate">
+          <div className="border pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"></div>
+        </div>
+      </div>
+    </>
+  );
 }
